8efc89380c872c3fc21f3da535f7849bd71533fb
RischioCinico <100477808+RischioCinico@users.noreply.github.com>
2022-12-24 17:58:20 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-mv -f policies.json /usr/lib/firefox/distribution/policies.json
+cp -f policies.json /usr/lib/firefox/distribution/policies.json
 echo Copiato policies.json
 
-mv -f my-settings.js /usr/lib/firefox/defaults/pref/my-settings.js
+cp -f my-settings.js /usr/lib/firefox/defaults/pref/my-settings.js
 echo Copiato my-settings.js
 
-mv -f mozilla.cfg /usr/lib/firefox/mozilla.cfg
+cp -f mozilla.cfg /usr/lib/firefox/mozilla.cfg
 echo Copiato mozilla.cfg
