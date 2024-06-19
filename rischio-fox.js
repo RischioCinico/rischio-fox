8234399f906c8cf0384c13cbe28db3e6ffa5b24f
RischioCinico <100477808+RischioCinico@users.noreply.github.com>
2024-06-19 14:01:33 +0200

    Update rischio-fox.js

--- rischio-fox.js
+++ rischio-fox.js
@@ -10,25 +10,48 @@
 /*    ░     ░       ░ ░ ░      ░  ░  ░░     ░ ░               ░ ░  ░    ░   */
 /*                    ░                                                     */
 
-/* scorciatoie newtab */
-lockPref("browser.newtabpage.pinned", "[{\"url\":\"https://github.com/RischioCinico/rischio-fox\",\"label\":\"127.4\"},{\"url\":\"https://www.youtube.com/\",\"label\":\"YouTube\"},{\"url\":\"https://www.reddit.com/\",\"label\":\"Reddit\"},{\"url\":\"https://mail.google.com/mail/u/0/?hl=it#inbox\",\"label\":\"Gmail\"},{\"url\":\"https://addons.mozilla.org/it/firefox/\",\"label\":\"Estensioni\"},{\"url\":\"https://wiki.archlinux.org/title/List_of_applications\",\"label\":\"Applicazioni\"},{\"url\":\"https://www.subito.it/\",\"label\":\"Subito\"},{\"url\":\"https://www.diretta.it/\",\"label\":\"Diretta\"}]");
+/*** about:newtab ***/
+/* siti nelle scorciatoie */
+lockPref("browser.newtabpage.pinned", "[{\"url\":\"https://github.com/RischioCinico/rischio-fox\",\"label\":\"127.5\"},{\"url\":\"https://www.youtube.com/\",\"label\":\"YouTube\"},{\"url\":\"https://www.reddit.com/\",\"label\":\"Reddit\"},{\"url\":\"https://mail.google.com/mail/u/0/?hl=it#inbox\",\"label\":\"Gmail\"},{\"url\":\"https://addons.mozilla.org/it/firefox/\",\"label\":\"Estensioni\"},{\"url\":\"https://wiki.archlinux.org/title/List_of_applications\",\"label\":\"Applicazioni\"},{\"url\":\"https://www.subito.it/\",\"label\":\"Subito\"},{\"url\":\"https://www.diretta.it/\",\"label\":\"Diretta\"}]");
+/* numero righe scorciatorie */
 lockPref("browser.newtabpage.activity-stream.topSitesRows", 1);
+/* mostra scorciatoie */
+lockPref("browser.newtabpage.activity-stream.feeds.topsites", true);
 lockPref("browser.newtabpage.activity-stream.feeds.section.topstories", false);
-/* ripristina searchbox in newtab */
+/* non spostare il focus dal searchbox alla barra degli indirizzi */
 clearPref("browser.newtabpage.activity-stream.improvesearch.handoffToAwesomebar");
 defaultPref("browser.newtabpage.activity-stream.improvesearch.handoffToAwesomebar", false);
+
+/*** popup e avvisi fastidiosi ***/
+/* disabilita il blocco popup integrato (sostituito da estensione apposita) */
+clearPref("dom.disable_open_during_load");
+lockPref("dom.disable_open_during_load", false);
+/* avviso about:config */
+clearPref("browser.aboutConfig.showWarning");
+defaultPref("browser.aboutConfig.showWarning", false);
 /* disabilita popup traduttore */
 clearPref("browser.translations.automaticallyPopup");
 defaultPref("browser.translations.automaticallyPopup", false);
-/* disabilita il blocco popup integrato */
-clearPref("dom.disable_open_during_load");
-lockPref("dom.disable_open_during_load", false);
-/* disabilita accessibilità */
-clearPref("accessibility.force_disabled");
-defaultPref("accessibility.force_disabled", 1);
 /* disabilita indicatori WebRTC (sono buggati) */
 clearPref("privacy.webrtc.hideGlobalIndicator");
 defaultPref("privacy.webrtc.hideGlobalIndicator", true);
+/** avviso schermo interno **/
+clearPref("full-screen-api.transition-duration.enter");
+clearPref("full-screen-api.transition-duration.leave");
+clearPref("full-screen-api.warning.delay");
+clearPref("full-screen-api.warning.timeout");
+defaultPref("full-screen-api.transition-duration.enter", "0 0");
+defaultPref("full-screen-api.transition-duration.leave", "0 0");
+defaultPref("full-screen-api.warning.delay", -1);
+defaultPref("full-screen-api.warning.timeout", 0);
+/* messaggio di benvenuto */
+clearPref("browser.aboutwelcome.enabled");
+defaultPref("browser.aboutwelcome.enabled", false);
+
+/*** altro ***/
+/* disabilita accessibilità */
+clearPref("accessibility.force_disabled");
+defaultPref("accessibility.force_disabled", 1);
 /* nascondi barra del titolo */
 defaultPref("browser.tabs.inTitlebar", 2);
 /* abilita scorrimento automatico */
@@ -159,16 +182,12 @@ clearPref("browser.privatebrowsing.vpnpromourl");
 clearPref("browser.shell.checkDefaultBrowser");
 clearPref("browser.clearPreferences.moreFromMozilla");
 clearPref("browser.tabs.tabmanager.enabled");
-clearPref("browser.aboutConfig.showWarning");
-clearPref("browser.aboutwelcome.enabled");
 
 defaultPref("browser.privatebrowsing.vpnpromourl", "");
 defaultPref("browser.vpn_promo.enabled", false)
 defaultPref("browser.shell.checkDefaultBrowser", false);
 defaultPref("browser.defaultPreferences.moreFromMozilla", false);
 defaultPref("browser.tabs.tabmanager.enabled", false);
-defaultPref("browser.aboutConfig.showWarning", false);
-defaultPref("browser.aboutwelcome.enabled", false);
 
 /** THEME ADJUSTMENTS **/
 clearPref("toolkit.legacyUserProfileCustomizations.stylesheets");
@@ -192,30 +211,12 @@ clearPref("cookiebanners.service.mode.privateBrowsing");
 defaultPref("cookiebanners.service.mode", 1);
 defaultPref("cookiebanners.service.mode.privateBrowsing", 1);
 
-/** FULLSCREEN NOTICE **/
-clearPref("full-screen-api.transition-duration.enter");
-clearPref("full-screen-api.transition-duration.leave");
-clearPref("full-screen-api.warning.delay");
-clearPref("full-screen-api.warning.timeout");
-
-defaultPref("full-screen-api.transition-duration.enter", "0 0");
-defaultPref("full-screen-api.transition-duration.leave", "0 0");
-defaultPref("full-screen-api.warning.delay", -1);
-defaultPref("full-screen-api.warning.timeout", 0);
-
 /** URL BAR **/
 clearPref("browser.urlbar.suggest.calculator");
 clearPref("browser.urlbar.unitConversion.enabled");
 
 defaultPref("browser.urlbar.suggest.calculator", true);
 defaultPref("browser.urlbar.unitConversion.enabled", true);
-
-/** NEW TAB PAGE **/
-clearPref("browser.newtabpage.activity-stream.feeds.topsites");
-clearPref("browser.newtabpage.activity-stream.feeds.section.topstories");
-
-defaultPref("browser.newtabpage.activity-stream.feeds.topsites", true);
-defaultPref("browser.newtabpage.activity-stream.feeds.section.topstories", false);
 
 /** POCKET **/
 clearPref("extensions.pocket.enabled");
