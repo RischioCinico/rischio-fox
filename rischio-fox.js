66470eba3441c8f1f600620adc128b9571df6950
RischioCinico <100477808+RischioCinico@users.noreply.github.com>
2025-05-28 03:07:32 +0200

    Update rischio-fox.js

--- rischio-fox.js
+++ rischio-fox.js
@@ -15,6 +15,9 @@
 defaultPref("browser.newtabpage.pinned", "[{\"url\":\"https://www.youtube.com/\",\"label\":\"YouTube\"},{\"url\":\"https://www.reddit.com/\",\"label\":\"Reddit\"},{\"url\":\"https://mail.google.com/mail/u/0/?hl=it#inbox\",\"label\":\"Gmail\"},{\"url\":\"https://addons.mozilla.org/it/firefox/\",\"label\":\"Estensioni\"},{\"url\":\"https://wiki.archlinux.org/title/List_of_applications\",\"label\":\"Applicazioni\"},{\"url\":\"https://it.wikipedia.org/wiki/Pagina_principale\",\"label\":\"Wikipedia\"},{\"url\":\"https://github.com/\",\"label\":\"GitHub\"},{\"url\":\"https://www.diretta.it/\",\"label\":\"Diretta\"}]");
 /* imposta lo sfondo predefinito */
 defaultPref("browser.newtabpage.activity-stream.newtabWallpapers.wallpaper", "dark-landscape");
+/* permetti sfondi personalizzati */
+defaultPref("browser.newtabpage.activity-stream.newtabWallpapers.customColor.enabled", true);
+defaultPref("browser.newtabpage.activity-stream.newtabWallpapers.customWallpaper.enabled", true)
 
 /*** Funzioni ***/
 /* abilita scorrimento automatico */
